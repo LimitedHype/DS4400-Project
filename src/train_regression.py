2f9ace1c1eb6faf696c2c5b58c3e427c4987060e
Peter Li <li.peter7122@gmail.com>
2022-11-29 16:17:07 -0500

    Changed file structure

---
+++ src/train_regression.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d  
+import sklearn as sk
+
+class TrainRegression:
+    def __init__(self):
+        pass
+    def train(self):
+        pass
+    def cross_test(self):
+        pass
