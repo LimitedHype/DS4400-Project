38e781934fe7a628369a41d0867c88985acc1ed6
Peter Li <li.peter7122@gmail.com>
2022-12-12 21:18:26 -0500

    Saving numpy files

--- dataframe/save.py
+++ dataframe/save.py
@@ -1,9 +1,19 @@
 import pandas as pd
+import numpy as np
 
-def loadDataFrame(path: str, num_rows: int) -> None:
-    df = pd.read_csv(path, nrows=num_rows)
-    print(df.to_string())
-    return None
+path = '../datasets/Bus Arrival Departure Times Apr-June 2020.csv'
 
-loadDataFrame('../datasets/Bus Arrival Departure Times Apr-June 2020.csv', 50)
+def loadDataFrame(filter_id: str, path: str, chunksize_: int) -> None:
+    new_frames = pd.DataFrame()
+    for chunk in pd.read_csv(path, chunksize=chunksize_):
+        new_frame = chunk[chunk.time_point_id == filter_id]
+        pd.concat([new_frames, new_frame])
+        # print(new_frame)
+    return new_frames
 
+def save_as_numpy(fileName: str, df):
+    new_df = df.to_numpy()
+    np.save(fileName, new_df)
+
+new_dataFrame = loadDataFrame(filter_id='nuniv', path=path, chunksize_=1000)
+save_as_numpy('../dataset_filtered/bus_arrival_departure_northeastern_april_june', new_dataFrame)
