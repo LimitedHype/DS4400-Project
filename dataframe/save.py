1d170d5e11a5611df68e7087678bd89fd79b0def
Peter Li <li.peter7122@gmail.com>
2022-12-15 23:16:25 -0500

    Making features

--- dataframe/save.py
+++ dataframe/save.py
@@ -6,32 +6,32 @@ RANDOM_SEED = 4400
 
 # Bus Arrival and Departure Time Datasets
 
-apr_june = ('../datasets/Bus Arrival Departure Times Apr-June 2020.csv', 'apr_june')
+sample_frames = ('../datasets/Bus Arrival Departure Times Apr-June 2020.csv', 'apr_june')
 jan_mar = ('../datasets/Bus Arrival Departure Times Jan-Mar 2020.csv', 'jan_mar')
 jul_sep = ('../datasets/Bus Arrival Departure Times Jul-Sep 2020.csv', 'jul_sep')
 oct_dec = ('../datasets/Bus Arrival Departure Times Oct-Dec 2020.csv', 'oct_dec')
 
 # Names
-bus_arrival_departure_list = [apr_june, jan_mar, jul_sep, oct_dec]
+bus_arrival_departure_list = [sample_frames, jan_mar, jul_sep, oct_dec]
 
-def loadDataFrame(filter_id: str, path: str, chunksize_: int) -> None:
+
+def loadDataFrame(path: str, chunksize_: int) -> None:
     print('Loading: ' + str(path))
     new_frames = pd.DataFrame()
     for i, chunk in enumerate(pd.read_csv(path, chunksize=chunksize_)):
         try:
-            new_frame = chunk[(chunk.time_point_id == filter_id) & (pd.notnull(chunk.actual))]
+            new_frame = chunk[pd.notnull(chunk.actual)]
             new_frames = pd.concat([new_frames, new_frame])
         except:
             pass
     return new_frames
 
-def loadDataFrameOct(filter_id: str, path: str, chunksize_: int) -> None:
+def loadDataFrameOct(path: str, chunksize_: int) -> None:
     print('Loading: ' + str(path))
     new_frames = pd.DataFrame()
     for i, chunk in enumerate(pd.read_csv(path, chunksize=chunksize_)):
         try:
-            
-            new_frame = chunk[(chunk.Timepoint == filter_id) & (pd.notnull(chunk.Actual))]
+            new_frame = chunk[pd.notnull(chunk.Actual)]
             new_frame = new_frame.rename(columns={"ServiceDate" : "service_date" ,"Route" : "route_id","Direction" : "direction", "HalfTripId" : "half_trip_id","Stop" : "stop_id","Timepoint" : "time_point_id","TimepointOrder" : "time_point_order","PointType" : "point_type","StandardType" : "standard_type","Scheduled" : "scheduled","Actual" : "actual","ScheduledHeadway" :"scheduled_headway" ,"Headway" : "headway"})
             new_frames = pd.concat([new_frames, new_frame])
         except:
@@ -44,11 +44,13 @@ def loadDataFrameOct(filter_id: str, path: str, chunksize_: int) -> None:
 
 list_of_path_parsed = []
 
+# filter_id = ['nuniv', 'brghm', 'hlong', 'shunt', 'fhill', 'hunbv', 'bbsta', 'jpctr', 'heath']
+
 for path, name in bus_arrival_departure_list:
     if name == 'oct_dec':
-        new_dataFrame = loadDataFrameOct(filter_id='nuniv', path=path, chunksize_=1000)
+        new_dataFrame = loadDataFrameOct(path=path, chunksize_=1000)
     else:
-        new_dataFrame = loadDataFrame(filter_id='nuniv', path=path, chunksize_=1000)
+        new_dataFrame = loadDataFrame(path=path, chunksize_=1000)
     print("Saving: " + str(path))
     new_dataFrame.to_csv('../dataset_filtered/bus_arrival_departure_northeastern_' + str(name) + '.csv')
     new_tuple = '../dataset_filtered/bus_arrival_departure_northeastern_' + str(name) + '.csv', 'name'
@@ -58,10 +60,19 @@ sample_frames = pd.DataFrame()
 for path, name in list_of_path_parsed:
     for i, chunk in enumerate(pd.read_csv(path, chunksize=1000)):
         try:
-            sample_frame = chunk.sample(frac=0.25, replace=False, random_state=RANDOM_SEED)
+            sample_frame = chunk.sample(frac=0.01, replace=False, random_state=RANDOM_SEED)
             sample_frames = pd.concat([sample_frames, sample_frame])
         except:
             pass
 
+# Convert to DateTime
+sample_frames['scheduled'] = pd.to_datetime(sample_frames['scheduled'])
+sample_frames['actual'] = pd.to_datetime(sample_frames['actual'])
+sample_frames['actual'] = (sample_frames['actual'] - sample_frames['actual'].dt.normalize()).dt.total_seconds()
+sample_frames['scheduled'] = (sample_frames['scheduled'] - sample_frames['scheduled'].dt.normalize()).dt.total_seconds()
+
 print("Saving sampled frames!")
 sample_frames.to_csv('../dataset_filtered/bus_arrival_departure_northeastern_sampled' + '.csv')
+
+
+
